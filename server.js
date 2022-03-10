670f93155c62307be6a5e930bfadb075d7f24d33
Martin <martingauna2000@gmail.com>
2022-03-09 22:27:44 -0300

    path proyect node

--- server.js
+++ server.js
@@ -3,12 +3,16 @@ const express = require('express');
 const admin = require('firebase-admin');
 const bcrypt = require('bcrypt');
 const path = require('path');
+//declare la ruta estatica
+let staticPacth = path.join(__dirname, "public");
 //inicializamos express.js
 const app = express();
+//middlewares
+app.use(express.static(staticPacth));
 //routes
 //home route
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
+    res.sendFile(path.join(staticPacth, "index.html"));
 })
 
 app.listen(3000, () => {

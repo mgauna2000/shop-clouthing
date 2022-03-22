48bb1190cd1792932accf2c4dbb7903046840d93
Martin <martingauna2000@gmail.com>
2022-03-21 22:07:55 -0300

    js form signup

---
+++ public/js/form.js
@@ -0,0 +1,24 @@
+//seleccionamos los inputs
+const submitBtn = document.querySelector('.submit-btn');
+const name = document.querySelector('#name');
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+const number = document.querySelector('#number');
+const tac = document.querySelector('terms-and-cond');
+const notification = document.querySelector('#notification');
+
+submitBtn.addEventListener('click', () => {
+    if (name.value.length < 3) {
+        showAlert('name must be 3 letters long');
+    }
+})
+//funcion de alerta
+const showAlert = (msg) => {
+    let alertBox = document.querySelector('.alert-box');
+    let alertMsg = document.querySelector('.alert-msg');
+    alertMsg.innerHTML = msg;
+    alertBox.classList.add('show');
+    setTimeout(() => {
+        alertBox.classList.remove('show');
+    }, 3000);
+}

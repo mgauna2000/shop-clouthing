00236a66fc4ae553b508d5ad89c91f51146d5854
Martin <martingauna2000@gmail.com>
2022-04-01 23:11:11 -0300

    compare token

--- public/js/token.js
+++ public/js/token.js
@@ -2,10 +2,23 @@ let char = `123abcde.fmnopqlABCDE@FJKLMNOPQRSTUVWXYZ456789stuvwxyz0!#$%&ijkrgh'*
 
 const generateToken = (key) => {
     let token = '';
-    for (let i = 0; i < key.length; i=i+2) {
+    for (let i = 0; i < key.length; i++) {
         let index = char.indexOf(key[i]) || char.length / 2;
         let randomIndex = Math.floor(Math.random() * index);
         token += char[randomIndex] + char[index - randomIndex];
     }
     return token;
+}
+
+const compareToken = (token, key) => {
+    let string = '';
+    for(let i = 0; i < token.length; i=i+2) {
+        let index1 = char.indexOf(token[i]);
+        let index2 = char.indexOf(token[i+1]);
+        string += char[index1 + index2];
+    }
+    if (string === key) {
+        return true;
+    }
+    return false;
 }

029c91d4c0c199250168158468e10fd73e62c065
Martin <martingauna2000@gmail.com>
2022-04-03 22:07:44 -0300

    add logout html

--- public/js/token.js
+++ public/js/token.js
@@ -22,3 +22,38 @@ const compareToken = (token, key) => {
     }
     return false;
 }
+// funciones comunes
+//funcion enviar datos
+const sendData = (path, data) => {
+    fetch(path, {
+        method: 'post',
+        headers: new Headers({'Content-Type': 'application/json'}),
+        body: JSON.stringify(data)
+    }).then((res) => res.json())
+    .then(response => {
+        processData(response);
+    })
+}
+
+const processData = (data) => {
+    loader.style.display = null;
+    if (data.alert) {
+        showAlert(data.alert);
+    }else if(data.name) {
+        //crear authToken
+        data.authToken = generateToken(data.email);
+        sessionStorage.user = JSON.stringify(data);
+        location.replace('/');
+    }
+}
+
+//funcion de alerta
+const showAlert = (msg) => {
+    let alertBox = document.querySelector('.alert-box');
+    let alertMsg = document.querySelector('.alert-msg');
+    alertMsg.innerHTML = msg;
+    alertBox.classList.add('show');
+    setTimeout(() => {
+        alertBox.classList.remove('show');
+    }, 3000);
+}
